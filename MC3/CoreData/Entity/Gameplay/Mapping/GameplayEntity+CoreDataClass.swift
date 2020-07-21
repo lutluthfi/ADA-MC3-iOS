d6e6e8c5f7a5d355958dc652b4600ed526785f41
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-07-21 15:21:52 +0700

    Add CoreDataStorage

---
+++ MC3/CoreData/Entity/Gameplay/Mapping/GameplayEntity+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  GameplayEntity+CoreDataClass.swift
+//  MC3
+//
+//  Created by Arif Luthfiansyah on 19/07/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(GameplayEntity)
+public class GameplayEntity: NSManagedObject {
+
+}
