6d7204fc4593b5a253465f1bea94745978b41791
Jeremy Jason <booya1402@gmail.com>
2020-07-22 17:48:48 +0700

    fix conflict

---
+++ MC3/Application/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  MC3
+//
+//  Created by Arif Luthfiansyah on 14/07/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+
+//        self.window = UIWindow(frame: UIScreen.main.bounds)
+//        let storyboard = UIStoryboard(name: AgreementViewController.identifier, bundle: nil)
+//        let viewController = storyboard
+//            .instantiateViewController(identifier: AgreementViewController.identifier)
+//        self.window?.rootViewController = viewController
+//        self.window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+
+}
+
