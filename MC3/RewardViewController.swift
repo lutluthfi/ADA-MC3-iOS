25a1d54a10d8888cd3b6a5c9d00b2a3a7e956b78
antonaulia <antoniusferiantoaulia@gmail.com>
2020-07-23 15:06:30 +0700

    .Start

---
+++ MC3/RewardViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RewardViewController.swift
+//  MC3
+//
+//  Created by Antonius F Aulia on 23/07/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class RewardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //di sini develop ton ! luy 
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
