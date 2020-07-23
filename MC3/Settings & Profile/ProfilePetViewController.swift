10efa94185198202d828bb10ce42bb1f4fa51019
Rahmat Zulfikri <me@rahmatzulfikri.xyz>
2020-07-23 15:22:57 +0700

    add profile pet

--- MC3/Settings & Profile/ProfilePetViewController.swift
+++ MC3/Settings & Profile/ProfilePetViewController.swift
@@ -10,13 +10,38 @@ import UIKit
 
 class ProfilePetViewController: UIViewController {
 
+    @IBOutlet var backBtn: UIButton!
+    @IBOutlet var homeBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        prepareScreen()
         // Do any additional setup after loading the view.
     }
     
-
+    func prepareScreen() {
+        homeBtn.layer.borderWidth = 4
+        homeBtn.layer.cornerRadius = 5
+        homeBtn.layer.borderColor = UIColor(hex: "#413834")?.cgColor
+        
+        backBtn.layer.borderWidth = 4
+        backBtn.layer.cornerRadius = 5
+        backBtn.layer.borderColor = UIColor(hex: "#413834")?.cgColor
+      }
+    
+    @IBAction func onPressButton(_ sender: UIButton) {
+        switch sender {
+        case homeBtn:
+            dismiss(animated: true, completion: nil)
+            break
+        case backBtn:
+            dismiss(animated: true, completion: nil)
+            break
+        default:
+            break
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
