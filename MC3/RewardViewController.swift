a4c86f3c79a0a6e847557d13b5fe10fceb791c40
antonaulia <antoniusferiantoaulia@gmail.com>
2020-07-23 16:58:43 +0700

    custom Seg-Control

--- MC3/RewardViewController.swift
+++ MC3/RewardViewController.swift
@@ -10,15 +10,73 @@ import UIKit
 
 class RewardViewController: UIViewController {
 
+    @IBOutlet weak var prizeButton: UIButton!
+    @IBOutlet weak var redeemButton: UIButton!
+    @IBOutlet weak var historyButton: UIButton!
+    
+    @IBAction func PrizeButtonPressed(_ sender: UIButton) {
+        prizeButton.isSelected = true
+        redeemButton.isSelected = false
+        historyButton.isSelected = false
+        buttonCondition()
+    }
+    
+    @IBAction func RedeemButtonPressed(_ sender: UIButton) {
+        prizeButton.isSelected = false
+        redeemButton.isSelected = true
+        historyButton.isSelected = false
+        buttonCondition()
+    }
+    
+    @IBAction func HistoryButtonPressed(_ sender: UIButton) {
+        prizeButton.isSelected = false
+        redeemButton.isSelected = false
+        historyButton.isSelected = true
+        buttonCondition()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //di sini develop ton ! luy 
-
+        prizeButton.isSelected = true
+        buttonCondition()
         // Do any additional setup after loading the view.
     }
     
-
+    func buttonCondition() {
+        prizeButton.setTitleColor(.white, for: .normal)
+        prizeButton.backgroundColor = #colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1)
+        prizeButton.layer.cornerRadius = 10
+        prizeButton.clipsToBounds = true
+        redeemButton.setTitleColor(.white, for: .normal)
+        redeemButton.backgroundColor = #colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1)
+        redeemButton.layer.cornerRadius = 10
+        redeemButton.clipsToBounds = true
+        historyButton.setTitleColor(.white, for: .normal)
+        historyButton.backgroundColor = #colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1)
+        historyButton.layer.cornerRadius = 10
+        historyButton.clipsToBounds = true
+        
+        if prizeButton.isSelected {
+            prizeButton.backgroundColor = #colorLiteral(red: 1, green: 0.9918932319, blue: 0.9789504409, alpha: 1)
+            prizeButton.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            prizeButton.layer.borderColor = #colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1)
+            prizeButton.layer.borderWidth = 4
+            prizeButton.setTitleColor(#colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1), for: .selected)
+        } else if redeemButton.isSelected {
+            redeemButton.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            redeemButton.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            redeemButton.layer.borderColor = #colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1)
+            redeemButton.layer.borderWidth = 4
+            redeemButton.titleLabel?.textColor = #colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1)
+            redeemButton.setTitleColor(#colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1), for: .selected)
+        } else if historyButton.isSelected{
+            historyButton.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            historyButton.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            historyButton.layer.borderColor = #colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1)
+            historyButton.layer.borderWidth = 4
+            historyButton.titleLabel?.textColor = #colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1)
+            historyButton.setTitleColor(#colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1), for: .selected)
+        }
+    }
     /*
     // MARK: - Navigation
 
