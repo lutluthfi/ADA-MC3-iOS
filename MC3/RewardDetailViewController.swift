292da02b1830fd687d44f0b0e953bcc89967015a
antonaulia <antoniusferiantoaulia@gmail.com>
2020-07-29 16:58:04 +0700

    kerja sama kesayangan

--- MC3/RewardDetailViewController.swift
+++ MC3/RewardDetailViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
+struct rewardDetail{
+    var rewardTitle :String
+    var rewardDesc : String
+    var rewardImage : UIImage
+}
+var rewardDetailArray = [rewardDetail(rewardTitle: "Safari",
+                                      rewardDesc: "Place: Taman Safari Indonesia \nActivities: Witness the majestic wildlife of Indonesia, an experience like no other (free lunch included). \nPrice: $150",
+                                      rewardImage: #imageLiteral(resourceName: "Safari-1")),
+                         rewardDetail(rewardTitle: "Seminar",
+                                      rewardDesc: "Topic: Smart Socialization Puppy \nDetails: Come learn about Socialization, what it is, what you need to do, and what you need to avoid. \nPrice: $100",
+                                      rewardImage: #imageLiteral(resourceName: "Seminar-1")),
+                         rewardDetail(rewardTitle: "Donation",
+                                      rewardDesc: "Cause: Help Charlie to do a Surgery \nDetails: Charlie has two fractures on his hip due to falling onto something with a sharp edge. Please send positive vibes his way.",
+                                      rewardImage: #imageLiteral(resourceName: "Donation-1"))]
+
+
 class RewardDetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var imageDetailLabel: UIImageView!
+    @IBOutlet weak var imageDetail: UIImageView!
     @IBOutlet weak var detailLabel: UILabel!
     @IBOutlet weak var buyButton: UIButton!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         buyButton.layer.cornerRadius = 10
         buyButton.clipsToBounds = true
-
+        
+        imageDetail.layer.borderWidth = 5
+        imageDetail.layer.borderColor = #colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1)
+        imageDetail.layer.cornerRadius = 15
+        imageDetail.clipsToBounds = true
+        
+        titleLabel.text = rewardDetailArray[selectedReward].rewardTitle
+        detailLabel.text = rewardDetailArray[selectedReward].rewardDesc
+        imageDetail.image = rewardDetailArray[selectedReward].rewardImage
         // Do any additional setup after loading the view.
     }
     
--- MC3/RewardViewController.swift
+++ MC3/RewardViewController.swift
@@ -8,19 +8,41 @@
 
 import UIKit
 
+struct Reward{
+    var rewardName : String
+    var rewardImage : UIImage
+}
+var rewardArray = [Reward(rewardName: "Safari", rewardImage: #imageLiteral(resourceName: "Safari-1")),
+                   Reward(rewardName: "Seminar", rewardImage: #imageLiteral(resourceName: "Seminar-1")),
+                   Reward(rewardName: "Donation", rewardImage: #imageLiteral(resourceName: "Donation-1")),
+                   Reward(rewardName: "Safari", rewardImage: #imageLiteral(resourceName: "Safari-2")),
+                   Reward(rewardName: "Seminar", rewardImage: #imageLiteral(resourceName: "Seminar-2")),
+                   Reward(rewardName: "Donation", rewardImage: #imageLiteral(resourceName: "Donation-2")),
+                   Reward(rewardName: "Seminar", rewardImage: #imageLiteral(resourceName: "Seminar-3")),
+                   Reward(rewardName: "Donation", rewardImage: #imageLiteral(resourceName: "Donation-3"))]
+
+var selectedReward = 0
+
 class RewardViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource {
     @IBOutlet weak var rewardCollectionView: UICollectionView!
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 15
+        return rewardArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = rewardCollectionView.dequeueReusableCell(withReuseIdentifier: "rewardCell", for: indexPath) as! RewardCollectionViewCell
+        cell.rewardCellLabel.text = rewardArray[indexPath.row].rewardName
+        cell.rewardCellImage.image = rewardArray[indexPath.row].rewardImage
         cell.rewardCellImage.layer.borderColor = #colorLiteral(red: 0.3243855536, green: 0.2837193608, blue: 0.2650057673, alpha: 1)
         cell.rewardCellImage.layer.cornerRadius = 10
         cell.rewardCellImage.layer.borderWidth = 3
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedReward = indexPath.row
+        performSegue(withIdentifier: "ToDetailReward", sender: self)
     }
     
 
