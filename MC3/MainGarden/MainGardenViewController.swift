452ebb7e2f259d34b2ce6e21928f508c62a376d7
Rahmat Zulfikri <me@rahmatzulfikri.xyz>
2020-08-01 09:39:29 +0700

    add unwind segue from profile & pet profile screen

--- MC3/MainGarden/MainGardenViewController.swift
+++ MC3/MainGarden/MainGardenViewController.swift
@@ -63,6 +63,11 @@ class MainGardenViewController: UIViewController {
             .animalInfo(dialogId: dialogId, delegate: nil)
         self.dialogFactory.show(scene: dialogScene)
     }
+    
+    // NEEDED! Do not delete!
+    @IBAction func unwindToMainGardenView(_ segue : UIStoryboardSegue) {
+      // Do nothing
+    }
 
 }
 
