7e5338c65a91d0f1c04508b1cbe10791243a81da
antonaulia <antoniusferiantoaulia@gmail.com>
2020-08-02 17:04:09 +0700

    kerjaan ditemenin velia SEKSEH, GAJADI KE PASAR BARU

---
+++ MC3/Rewards & Shop/Shop/ShopCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ShopCollectionViewCell.swift
+//  MC3
+//
+//  Created by Antonius F Aulia on 02/08/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class ShopCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var shopCellImage: UIImageView!
+    
+    @IBOutlet weak var shopCellLabel: UILabel!
+}
